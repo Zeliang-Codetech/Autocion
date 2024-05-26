11b1dc293358cb5a49bbd0e5222d98341f4013d0
Moachuba Jamir <87128349+Moachuba-Jamir@users.noreply.github.com>
2024-05-26 22:05:30 +0530

    Update AdminRoute.jsx

--- Frontend/Routes/AdminRoute.jsx
+++ Frontend/Routes/AdminRoute.jsx
@@ -1,36 +1,36 @@
-"use client";
-
-import { useRouter } from "next/navigation";
-import Cookies from "js-cookie";
-
-const PrivateRoute = ({ children }) => {
-  const isAuthenticated = Cookies.get("admin");
-  const router = useRouter();
-  if (!isAuthenticated) {
-    router.push("/admin");
-  }
-  return children;
-};
-export default PrivateRoute;
-
-
 // "use client";
 
-// import { useEffect } from "react";
 // import { useRouter } from "next/navigation";
 // import Cookies from "js-cookie";
 
 // const PrivateRoute = ({ children }) => {
+//   const isAuthenticated = Cookies.get("admin");
 //   const router = useRouter();
-
-//   useEffect(() => {
-//     const isAuthenticated = Cookies.get("admin");
-//     if (!isAuthenticated) {
-//       router.push("/admin");
-//     }
-//   }, [router]);
-
-//   return isAuthenticated ? children : null;
+//   if (!isAuthenticated) {
+//     router.push("/admin");
+//   }
+//   return children;
 // };
-
 // export default PrivateRoute;
+
+
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import Cookies from "js-cookie";
+
+const PrivateRoute = ({ children }) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const isAuthenticated = Cookies.get("admin");
+    if (!isAuthenticated) {
+      router.push("/admin");
+    }
+  }, [router]);
+
+  return isAuthenticated ? children : null;
+};
+
+export default PrivateRoute;
