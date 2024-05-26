10202b23aa6ffd741815a886f9574d6dddfc553a
Moachuba Jamir <87128349+Moachuba-Jamir@users.noreply.github.com>
2024-05-26 22:10:20 +0530

    Update AdminRoute.jsx

--- Frontend/Routes/AdminRoute.jsx
+++ Frontend/Routes/AdminRoute.jsx
@@ -21,16 +21,16 @@ import { useRouter } from "next/navigation";
 import Cookies from "js-cookie";
 
 const PrivateRoute = ({ children }) => {
-  const router = useRouter();
-
   useEffect(() => {
+    const router = useRouter();
     const isAuthenticated = Cookies.get("admin");
     if (!isAuthenticated) {
       router.push("/admin");
     }
+      return isAuthenticated ? children : null;
   }, [router]);
 
-  return isAuthenticated ? children : null;
+
 };
 
 export default PrivateRoute;
