b0bc87440e89382ebe528f4ad806cceabe2d6f2b
Moachuba Jamir <87128349+Moachuba-Jamir@users.noreply.github.com>
2024-05-26 17:11:04 +0530

    Update page.jsx
    Added useEffect hook to render Browser specific API's like local storage to ensure it execution only on client side and not server side

--- Frontend/app/(root)/change-password/page.jsx
+++ Frontend/app/(root)/change-password/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./style.scss";
 import { useRouter } from "next/navigation";
 import axios from "axios";
