e1334ad21a4b30d02d81599cd5f0c6d4398765e4
Moachuba Jamir <87128349+Moachuba-Jamir@users.noreply.github.com>
2024-05-27 22:14:42 +0530

    Update next.config.js

--- Frontend/next.config.js
+++ Frontend/next.config.js
@@ -3,7 +3,6 @@ const nextConfig = {
   images: {
     domains: ["localhost"],
   },
-
   images: {
     unoptimized: true,
   },
@@ -17,6 +16,21 @@ const nextConfig = {
     TEMPLATE_CODE_2: process.env.TEMPLATE_CODE_2,
     TOKEN: process.env.TOKEN,
   },
-};
+  
+  async headers() {
+      return [
+          {
+          // matching all API routes
+          source: "/api/:path*",
+            headers: [
+                { key: "Access-Control-Allow-Credentials", value: "true" },
+                { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+                { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+            ]
+          }
+        ]
+    }
+}
 
 module.exports = nextConfig;
