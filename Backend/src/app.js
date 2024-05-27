e9a7a28645087020b23e9c02cd1189f5f1e66a19
Moachuba Jamir <87128349+Moachuba-Jamir@users.noreply.github.com>
2024-05-27 21:54:19 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -22,7 +22,7 @@ const app = express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 // not working for some reason 
-app.use(cors());
+// app.use(cors());
 
 // hardcoded 
 app.use(
