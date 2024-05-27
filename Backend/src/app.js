98036cb0884928779e6dc3c56940bb771294aae6
Moachuba Jamir <87128349+Moachuba-Jamir@users.noreply.github.com>
2024-05-27 23:02:01 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -28,7 +28,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(
   cors({
     origin: ["http://localhost:3000", "https://autocion-s436.vercel.app"],
-    methods: [ "POST",  "GET" ],
+    methods: [ "POST", "GET", "PUT", "PATCH", "DELETE", "OPTIONS" ],
     credentials: true,
   })
 );
