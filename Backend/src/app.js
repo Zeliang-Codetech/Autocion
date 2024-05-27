2e5e345f0aec29933a60ff4e45775f9492c9b5ac
Moachuba Jamir <87128349+Moachuba-Jamir@users.noreply.github.com>
2024-05-27 22:42:26 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -27,14 +27,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // hardcoded 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://autocion-frontend-oflpf009d-moachubatomatos-projects.vercel.app/"],
+    origin: ["http://localhost:3000", "https://autocion-s436.vercel.app"],
     methods: [ "POST",  "GET" ],
     credentials: true,
   })
 );
 
   app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://autocion-frontend-oflpf009d-moachubatomatos-projects.vercel.app/");
+    res.header("Access-Control-Allow-Origin", "https://autocion-s436.vercel.app");
     res.header(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept, Authorization"
